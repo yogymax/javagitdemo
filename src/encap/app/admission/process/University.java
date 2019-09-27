e13cf1a39e21cddfd9b2db634f6a152d234352fc
yogymax <yogymax@gmail.com>
2019-09-27 18:51:05 +0530

    final changes

--- src/encap/app/admission/process/University.java
+++ src/encap/app/admission/process/University.java
@@ -7,6 +7,7 @@ public class University {
 		College c=new College();
 		Student[] students=c.admission();
 	
+		System.out.println("inside university file changes..");
 		for (int i = 0; i < students.length; i++) {
 			Student student=students[i];
 			System.out.println(student.getRollNo()+"\t"+student.getName()+"\t"+student.getAddress()+"\t"+student.getMobile());
